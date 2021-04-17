8bc0f6b81d78d2d6c47c677565afeec838eb8f7a
vijay75740 <57560561+vijay75740@users.noreply.github.com>
2021-04-17 18:04:28 +0530

    Update index.js
    api interval

--- routes/index.js
+++ routes/index.js
@@ -56,6 +56,19 @@ function postImageWidth(post_link,token) {
       .catch(console.error);
     }
 
+setInterval( function setup() {
+  let sqlsss = "SELECT * FROM post_flags";
+  connection.query(sqlsss, function (err, tagChangeRandom) {
+    console.log('tagChangeRandom: ', tagChangeRandom[0].tag_switch);
+    var a = moment().utcOffset("+05:30").format("HH:mm");
+    if(a == "02:00"||a == "04:00"||a == "06:00"||a == "08:00"||a == "10:00"||a == "12:00"||a == "14:00"||a == "16:00"||a == "18:00"||a == "20:00"||a == "22:00"||a == "23:59" ){
+      console.log("a");
+    }else{
+      console.log("b");
+    }
+      })
+}, 19000)
+
     
     // function postImageWidth(post_link,token) {
 function postFlipkartImageWidth(post_link,token) {
